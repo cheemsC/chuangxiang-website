d7abada70573feb945cef67402e61fe235559798
cheemsC <2985057233@qq.com>
2026-09-05 03:56:42 +0800

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,13 +4,13 @@ window.SITE_CONFIG = {
   companyName2: "宁波得翔电子有限公司",
   brandName: "创翔电子 · 得翔电子",
   companyShort: "创翔电子",
-  slogan: "专注连接器、电源插座与接线端子制造",
+  slogan: "专注连接器、电源插座、各国标准电源线与接线端子制造",
   subSlogan: "支持多系列、多规格产品供应及 OEM / ODM 配套",
   phone: "0574-63552827",
   mobile1: "13600615234",
   mobile2: "13505844995",
   email: "172671662@qq.com",
-  address: "宁波中意工业园区（余姚小曹娥镇）万洋众创城，海涛路10号15幢楼",
+  address: "浙江省宁波市余姚市小曹娥镇中意万洋众创城2期15幢",
   established: "2006",
   certifications: "ISO9001 · ISO14001 · UL · VDE · CCC · CQC · CE · CB · RoHS"
 };
